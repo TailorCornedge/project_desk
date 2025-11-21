db4477b3834729d66e735f2ab042b5fd96f1c722
TailorCornedge <szabok2@kkszki.hu>
2025-11-21 11:17:03 +0100

    menupont

---
+++ project_desk/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_desk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> e_mailok = new List<string>();
+            e_mailok.Add("alma1@kort.hu");
+            e_mailok.Add("alma2@korte.hu");
+            e_mailok.Add("alma3@kortu.hu");
+            e_mailok.Add("alma4@korta.hu");
+            e_mailok.Add("alma5@kortm.hu");
+            while (true) { 
+                Console.Clear();
+                Console.WriteLine("0. Kilépés");
+                Console.WriteLine("1. e-mailek listázása");
+                Console.WriteLine("2. új e-mail");
+                Console.WriteLine("3. e-mail törlése");
+                Console.WriteLine("4. érvényes e-mailok listázása");
+
+                string valasztas=Console.ReadLine();
+                switch (valasztas)
+                {
+                    case "0": return;
+                    case "1": Console.WriteLine("e-mailek listázása");
+                              
+                        break;
+                    case "2": Console.WriteLine("új e-mailek"); break;
+                    case "3": Console.WriteLine("e-mail törlés"); break;
+                    case "4": Console.WriteLine("érvényesek listázás"); break;
+                    default: Console.Clear(); Console.WriteLine("rossz parancs"); break;
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
