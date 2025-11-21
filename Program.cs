b4739ae24c84de7996033e8abb7ffdc544f54253
Lesummer <144891846+TailorCornedge@users.noreply.github.com>
2025-11-21 11:31:49 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_desk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> e_mailok = new List<string>();
+            e_mailok.Add("alma1@kort.hu");
+            e_mailok.Add("alma2@korte.hu");
+            e_mailok.Add("alma3@kortu.hu");
+            e_mailok.Add("alma4@korta.hu");
+            e_mailok.Add("alma5@kortm.hu");
+            while (true) { 
+                Console.Clear();
+                Console.WriteLine("0. Kilépés");
+                Console.WriteLine("1. e-mailek listázása");
+                Console.WriteLine("2. új e-mail");
+                Console.WriteLine("3. e-mail törlése");
+                Console.WriteLine("4. érvényes e-mailok listázása");
+
+                string valasztas=Console.ReadLine();
+                switch (valasztas)
+                {
+                    case "0": return;
+                    case "1": 
+                        Console.Clear();
+                        Console.WriteLine("e-mail címet:");
+                        int sorszam = 0;
+                        foreach(String e_mail in e_mailok)
+                        {
+                            Console.WriteLine($"{sorszam++} {e_mail}");
+                        }
+                        break;
+                    case "2":
+                        Console.Clear();
+                        Console.Write("új e-mailek");
+                        String uj = Console.ReadLine();
+                        e_mailok.Add(uj);
+                        break;
+                    case "3":
+                        Console.WriteLine("Adja meg a törlendő e-mail azonosítóját: ");
+                        int index = Convert.ToInt32(Console.ReadLine());
+                        e_mailok.RemoveAt(index);
+                        break;
+                    case "4":
+                        Console.WriteLine("Érvényesek e-mailok listája");
+                        foreach(String e_mail in e_mailok)
+                        {
+
+                        }
+                        break;
+                    default: Console.Clear(); Console.WriteLine("rossz parancs"); break;
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
